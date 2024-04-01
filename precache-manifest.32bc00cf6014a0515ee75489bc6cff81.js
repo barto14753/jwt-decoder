ef227a41ab8edaf261ad531ff258bcf8da09dd82
barto14753 <barto14753@users.noreply.github.com>
2024-04-01 10:04:52 +0000

    deploy: 0496da57522cafc2806dcca6d300572e78beefd0

---
+++ precache-manifest.32bc00cf6014a0515ee75489bc6cff81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef5807839d6867ceaab2d8096617bac",
+    "url": "/jwt-decoder/index.html"
+  },
+  {
+    "revision": "fd903b76f0d5b18a067d",
+    "url": "/jwt-decoder/static/css/main.c6001462.chunk.css"
+  },
+  {
+    "revision": "e75f5ec3f34705c39e74",
+    "url": "/jwt-decoder/static/js/2.f6988871.chunk.js"
+  },
+  {
+    "revision": "91982a67b60161113a7196b988be415a",
+    "url": "/jwt-decoder/static/js/2.f6988871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd903b76f0d5b18a067d",
+    "url": "/jwt-decoder/static/js/main.7a4dd86c.chunk.js"
+  },
+  {
+    "revision": "8de154f82dc8c14d5b1f",
+    "url": "/jwt-decoder/static/js/runtime-main.812ddc83.js"
+  }
+]);
